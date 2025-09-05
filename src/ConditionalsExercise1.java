c7dfb585390ad509ba41948104630ac4b5ead75c
Max Joseph <maxjoseph22@gmail.com>
2025-09-05 11:34:10 +0100

    first commit

---
+++ src/ConditionalsExercise1.java
@@ -0,0 +1,16 @@
+public class ConditionalsExercise1 {
+    public static void main(String[] args) {
+        int number = 18; // try changing this value
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+        // TODO: if number is divisible by 2, print "Even"
+        // otherwise print "Odd"
+
+
+
+    }
+}
---
+++ src/ConditionalsExercise2.java
@@ -0,0 +1,7 @@
+public class ConditionalsExercise2 {
+    public static void main(String[] args) {
+        int age = 20; // try different ages
+
+        // TODO: use if/else to check voting eligibility
+    }
+}
